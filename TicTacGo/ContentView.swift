cc32ddb05f09e73007a707d24f39cc078ed6b3c0
roanacla <roanacla@me.com>
2020-08-06 15:48:21 -0700

    Initial Commit

---
+++ TicTacGo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacGo
+//
+//  Created by Roger Navarro on 8/6/20.
+//  Copyright © 2020 Roger Navarro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
