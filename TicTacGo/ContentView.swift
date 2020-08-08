a2570300896393ed4ac257efd7d24f88817c8771
roanacla <roanacla@me.com>
2020-08-07 18:04:20 -0700

    Add Exercise and rest timers

--- TicTacGo/ContentView.swift
+++ TicTacGo/ContentView.swift
@@ -9,25 +9,73 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var currentTime: Date = Date()
+  @State private var exerSeconds: Int = 0
+  @State private var exerMinutes: Int = 0
+  @State private var restSeconds: Int = 0
+  @State private var restMinutes: Int = 0
+  
   let timer = ViewModel()
 
   var body: some View {
     VStack {
-      Button(action: self.start,
-             label: { Text("OK") }
-      )
-//      .onReceive(timer.source){ newCurrentTime in
-//        print(newCurrentTime)
-//      }
-    }
-    .onReceive(timer.currentTimePublisher){ newCurrentTime in
-      print(newCurrentTime)
+      GeometryReader { geometry in
+        VStack {
+          Text("Exercise time:")
+          HStack{
+            Picker(selection: self.$exerMinutes, label: Text("")) {
+              ForEach(0..<6) {
+                Text("\($0) min").tag($0)
+              }
+            }
+            .frame(maxWidth: geometry.size.width / 2)
+            Picker(selection: self.$exerSeconds, label: Text("")) {
+              ForEach(0..<60) {
+                Text("\($0) sec").tag($0)
+              }
+            }
+            .frame(maxWidth: geometry.size.width / 2)
+          }
+          Text("Rest time:")
+          HStack{
+            Picker(selection: self.$restMinutes, label: Text("")) {
+              ForEach(0..<6) {
+                Text("\($0) min").tag($0)
+              }
+            }
+            .frame(maxWidth: geometry.size.width / 2)
+            Picker(selection: self.$restSeconds, label: Text("")) {
+              ForEach(0..<60) {
+                Text("\($0) sec").tag($0)
+              }
+            }
+            .frame(maxWidth: geometry.size.width / 2)
+          }
+        }
+      }
+      HStack {
+        Button(action: self.start,
+               label: { Text("START") }
+        )
+        .padding()
+        Button(action: self.stop,
+               label: { Text("STOP")}
+        )
+        .padding()
+      }
+      .onReceive(timer.currentTimePublisher){ newCurrentTime in
+        print(newCurrentTime)
+      }
     }
   }
   
   func start() {
+    let lapTime = LapTime(exerciseMinutes: exerMinutes, exerciseSeconds: exerSeconds, restMinutes: restMinutes, restSeconds: restSeconds)
+    timer.lapTime = lapTime
     timer.startTimer()
+  }
+  
+  func stop() {
+    timer.stopTimer()
   }
 }
 
--- TicTacGo/ViewModel.swift
+++ TicTacGo/ViewModel.swift
@@ -9,16 +9,37 @@
 import Foundation
 import Combine
 
+public struct LapTime: CustomStringConvertible {
+  public var description: String {
+    return """
+    Exercise Minutes: \(exerciseMinutes)
+    Exercise Seconds: \(exerciseSeconds)
+    Rest Minutes: \(restMinutes)
+    Rest Seconds: \(restSeconds)
+    """
+  }
+  
+  var exerciseMinutes = 0
+  var exerciseSeconds = 0
+  var restMinutes = 0
+  var restSeconds = 0
+  
+  
+}
+
 public class ViewModel {
   let currentTimePublisher = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default)
   var cancellable: AnyCancellable?
-  var source = PassthroughSubject<Int, Never>()
+  var lapTime: LapTime?
   
   func startTimer() {
+    guard let lapTime = lapTime else { return }
+    print(lapTime.description)
     self.cancellable = currentTimePublisher.connect() as? AnyCancellable
-    source.send(1)
-    source.send(2)
-    source.send(3)
+  }
+  
+  func stopTimer() {
+    self.currentTimePublisher.connect().cancel()
   }
   
   deinit {
