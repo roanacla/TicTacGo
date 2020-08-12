7e5b9fc4689810846f9c01ee06990968384de502
roanacla <roanacla@me.com>
2020-08-11 19:20:02 -0700

    Fix the UI and add loop slider

--- TicTacGo/ContentView.swift
+++ TicTacGo/ContentView.swift
@@ -11,53 +11,65 @@ import SwiftUI
 struct ContentView: View {
   @ObservedObject var timer = ViewModel()
   @State var isStartButtonDisabled = false
+  @State var loops: Double = 1
 
   var body: some View {
     VStack {
+      Text("Exercise time:")
+        .padding()
       GeometryReader { geometry in
-        VStack {
-          Text("Exercise time:")
-          HStack{
-            Picker(selection: self.$timer.lapTime.exerciseMinutes, label: Text("")) {
-              ForEach(0..<6) {
-                Text("\($0) min").tag($0)
-              }
+        HStack{
+          Picker(selection: self.$timer.lapTime.exerciseMinutes, label: Text("")) {
+            ForEach(0..<6) {
+              Text("\($0) min").tag($0)
             }
-            .frame(maxWidth: geometry.size.width / 2)
-            Picker(selection: self.$timer.lapTime.exerciseSeconds, label: Text("")) {
-              ForEach(0..<60) {
-                Text("\($0) sec").tag($0)
-              }
-            }
-            .frame(maxWidth: geometry.size.width / 2)
           }
-          Text("Rest time:")
-          HStack{
-            Picker(selection: self.$timer.lapTime.restMinutes, label: Text("")) {
-              ForEach(0..<6) {
-                Text("\($0) min").tag($0)
-              }
+          .frame(maxWidth: geometry.size.width / 2)
+          Picker(selection: self.$timer.lapTime.exerciseSeconds, label: Text("")) {
+            ForEach(0..<60) {
+              Text("\($0) sec").tag($0)
             }
-            .frame(maxWidth: geometry.size.width / 2)
-            Picker(selection: self.$timer.lapTime.restSeconds, label: Text("")) {
-              ForEach(0..<60) {
-                Text("\($0) sec").tag($0)
-              }
-            }
-            .frame(maxWidth: geometry.size.width / 2)
           }
+          .frame(maxWidth: geometry.size.width / 2)
         }
       }
-      HStack {
-        Button(action: self.start,
-               label: { Text("START") }
-        )
-        .disabled(isStartButtonDisabled)
+      Text("Rest time:")
         .padding()
+      GeometryReader { geometry in
+        HStack{
+          Picker(selection: self.$timer.lapTime.restMinutes, label: Text("")) {
+            ForEach(0..<6) {
+              Text("\($0) min").tag($0)
+            }
+          }
+          .frame(maxWidth: geometry.size.width / 2)
+          Picker(selection: self.$timer.lapTime.restSeconds, label: Text("")) {
+            ForEach(0..<60) {
+              Text("\($0) sec").tag($0)
+            }
+          }
+          .frame(maxWidth: geometry.size.width / 2)
+        }
+      }
+      Text("Loops: \(Int(self.loops))")
+        .padding()
+      HStack {
+        Text("1")
+        Slider(value: self.$loops, in: 1...10, step: 1.0)
+        Text("10")
+      }
+      .padding(.leading)
+      .padding(.trailing)
+      HStack {
         Button(action: self.stop,
                label: { Text("STOP")}
         )
-        .padding()
+          .padding()
+        Button(action: self.start,
+               label: { Text("START") }
+        )
+          .disabled(self.isStartButtonDisabled)
+          .padding()
       }
     }
   }
